71fca4f317d183bbdf9b75204308afb995801854
Bernadette Prickett <bprickett@gmx.com>
2018-06-05 21:24:13 +0100

    Filling out the categories class and uplaoding an image

--- PictureBookV7/Areas/Admin/Controllers/ShopController.cs
+++ PictureBookV7/Areas/Admin/Controllers/ShopController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PictureBookV7.Models.Data;
+using PictureBookV7.Models.ViewModels.Pages.Shop;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +10,20 @@ namespace PictureBookV7.Areas.Admin.Controllers
 {
     public class ShopController : Controller
     {
-        // GET: Admin/Shop
-        public ActionResult Index()
+        // GET: Admin/Shop/Categories
+        public ActionResult Categories()
         {
-            return View();
+            //Declare a list of models
+            List<CategoryVM> categoryVMList;
+
+            using (Db db = new Db())
+            {
+                //Initialise the list
+                categoryVMList = db.Categories.ToArray().OrderBy(x => x.Sorting).Select(x => new CategoryVM(x)).ToList();
+            }
+
+                //Return view with a list
+                return View(categoryVMList);
         }
     }
 }
