3252faef35f5f3ca320af4f090b3aa97c7fe15b4
Bernadette Prickett <bprickett@gmx.com>
2018-06-05 20:44:57 +0100

    Added the ability to add media files

--- PictureBookV7/Models/ViewModels/Pages/PageVM.cs
+++ PictureBookV7/Models/ViewModels/Pages/PageVM.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace PictureBookV7.Models.ViewModels.Pages
 {
@@ -33,6 +34,7 @@ namespace PictureBookV7.Models.ViewModels.Pages
         public string Slug { get; set; }
         [Required]
         [StringLength(int.MaxValue, MinimumLength = 3)]
+        [AllowHtml]
         public string Body { get; set; }
         public int Sorting { get; set; }
         public bool HasSidebar { get; set; }
--- PictureBookV7/Models/ViewModels/Pages/SidebarVM.cs
+++ PictureBookV7/Models/ViewModels/Pages/SidebarVM.cs
@@ -3,12 +3,14 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace PictureBookV7.Models.ViewModels.Pages
 {
     public class SidebarVM
     {
         public int Id { get; set; }
+        [AllowHtml]
         public string Body { get; set; }
 
         public SidebarVM()
@@ -21,5 +23,7 @@ namespace PictureBookV7.Models.ViewModels.Pages
             Id = row.Id;
             Body = row.Body;
         }
+
+
     }
 }
