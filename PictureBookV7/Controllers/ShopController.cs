5d1de0aa45f39e9c923fcacfa895e543f78b58bb
Bernadette Prickett <bprickett@gmx.com>
2018-06-06 19:16:08 +0100

    Product details and some site.cc

--- PictureBookV7/Controllers/ShopController.cs
+++ PictureBookV7/Controllers/ShopController.cs
@@ -2,6 +2,7 @@
 using PictureBookV7.Models.ViewModels.Pages.Shop;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -52,14 +53,44 @@ namespace PictureBookV7.Controllers
                 ViewBag.CategoryName = productCat.CategoryName;
             }
 
-
-
-
-
-
-
             //Return view with a list
             return View(productVMList);
+        }
+
+        // GET: Shop/Product-Details/Name
+        [ActionName("Product-Details")]
+        public ActionResult ProductDetails(string name)
+        {
+            //Declare VM and DTO
+            ProductVM model;
+            ProductDTO dto;
+
+            //Initialise product id
+            int id = 0;
+
+            using (Db db = new Db())
+            {
+                //Check if product exists
+                if (! db.Products.Any(x => x.Slug.Equals(name)))
+                {
+                    return RedirectToAction("Index");
+                }
+
+                //Initialise productDTO
+                dto = db.Products.Where(x => x.Slug == name).FirstOrDefault();
+
+                //Get insert id
+                id = dto.Id;
+
+                //Initialise model
+                model = new ProductVM(dto);
+            }
+
+            //Get gallery images
+            model.GalleryImages = Directory.EnumerateFiles(Server.MapPath("~/Images/Uploads/Products/" + id + "/Gallery/Thumbs")).Select(fn => Path.GetFileName(fn));
+
+            //Return view with model
+            return View("ProductDetails", model);
         }
     }
 }
