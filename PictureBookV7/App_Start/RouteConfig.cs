b2c38e95397ead26a478fca71927885dd395bce0
Bernadette Prickett <bprickett@gmx.com>
2018-06-06 17:43:48 +0100

    Started front-end stuff

--- PictureBookV7/App_Start/RouteConfig.cs
+++ PictureBookV7/App_Start/RouteConfig.cs
@@ -13,11 +13,14 @@ namespace PictureBookV7
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            );
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "PictureBookV7.Controllers" });
+            routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "PictureBookV7.Controllers" });
+
+            //routes.MapRoute(
+            //    name: "Default",
+            //    url: "{controller}/{action}/{id}",
+            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            //);
         }
     }
 }
---
+++ PictureBookV7/Controllers/PagesController.cs
@@ -0,0 +1,59 @@
+﻿using PictureBookV7.Models.Data;
+using PictureBookV7.Models.ViewModels.Pages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PictureBookV7.Controllers
+{
+    public class PagesController : Controller
+    {
+        // GET: Index/{page}
+        public ActionResult Index(string page = "")
+        {
+            //Get/Set page slug(url)
+            if (page == "")            
+                page = "home";
+
+            //Declare model and DTO
+            PageVM model;
+            PageDTO dto;
+
+            //Check if page exists
+            using (Db db = new Db())
+            {
+                if (! db.Pages.Any(x => x.Slug.Equals(page)))
+                {
+                    return RedirectToAction("Index", new {page = ""});
+                }
+            }
+
+            //Get page DTO
+            using (Db db = new Db())
+            {
+                dto = db.Pages.Where(x => x.Slug == page).FirstOrDefault();
+            }
+
+            //Set page title
+            ViewBag.PageTitle = dto.Title;
+
+            //Check for sidebar
+            if (dto.HasSidebar == true)
+            {
+                ViewBag.Sidebar = "Yes";
+            }
+            else
+            {
+                ViewBag.Sidebar = "No";
+            }
+
+            //Initialise model
+            model = new PageVM(dto);
+
+            //Return view with model
+            return View(model);
+        }
+    }
+}
