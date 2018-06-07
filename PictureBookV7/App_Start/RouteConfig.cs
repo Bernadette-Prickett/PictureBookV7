13c169b6809808149fc72942392338f9ecb42a5f
Bernadette Prickett <bprickett@gmx.com>
2018-06-07 02:14:13 +0100

    Create Account

--- PictureBookV7/App_Start/RouteConfig.cs
+++ PictureBookV7/App_Start/RouteConfig.cs
@@ -13,22 +13,17 @@ namespace PictureBookV7
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            //routes.MapRoute("CategoryMenuPartial", "Shop/CategoryMenuPartial", new { controller = "Shop", action = "CategoryMenuPartial" }, new[] { "PictureBookV7.Controllers" });
+            routes.MapRoute("Account", "Account/{action}/{id}", new { controller = "Account", action = "Index", id = UrlParameter.Optional }, new[] { "CmsShoppingCart.Controllers" });
 
             routes.MapRoute("Cart", "Cart/{action}/{id}", new { controller = "Cart", action = "Index", id = UrlParameter.Optional }, new[] { "PictureBookV7.Controllers" });
+
             routes.MapRoute("Shop", "Shop/{action}/{name}", new { controller = "Shop", action = "Index", name = UrlParameter.Optional }, new[] { "PictureBookV7.Controllers" });
 
             routes.MapRoute("SidebarPartial", "Pages/SidebarPartial", new { controller = "Pages", action = "SidebarPartial" }, new[] { "PictureBookV7.Controllers" });
             routes.MapRoute("PagesMenuPartial", "Pages/PagesMenuPartial", new { controller = "Pages", action = "PagesMenuPartial" }, new[] { "PictureBookV7.Controllers" });
             routes.MapRoute("Pages", "{page}", new { controller = "Pages", action = "Index" }, new[] { "PictureBookV7.Controllers" });            
-            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "PictureBookV7.Controllers" });           
-            
+            routes.MapRoute("Default", "", new { controller = "Pages", action = "Index" }, new[] { "PictureBookV7.Controllers" });          
 
-            //routes.MapRoute(
-            //    name: "Default",
-            //    url: "{controller}/{action}/{id}",
-            //    defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-            //);
         }
     }
 }
---
+++ PictureBookV7/Controllers/AccountController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PictureBookV7.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Account/Create-Account
+        [ActionName("Create-Account")]
+        public ActionResult CreateAccount()
+        {
+            return View("CreateAccount");
+        }
+    }
+}
